7aea1eff06d3a6d8d54e9b7cd0ff54572efa8793
Ana Carolina Pagnoncelli <acpagnoncelli@inf.ufrgs.br>
2021-03-07 15:13:33 -0300

    Add Gyroscope Sensor

--- Activity4/app/src/main/java/com/example/activity3/MainActivity.kt
+++ Activity4/app/src/main/java/com/example/activity3/MainActivity.kt
@@ -13,22 +13,49 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var sensorManager: SensorManager? = null
-    private var light: Sensor? = null
+    private var mLightSensor: Sensor? = null
+    private var mGyroscopeSensor: Sensor? = null
     var lightValue: TextView? = null
+    var gyroscopeX: TextView? = null
+    var gyroscopeY: TextView? = null
+    var gyroscopeZ: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lightValue = findViewById<TextView>(R.id.light_id);
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager;
-        light = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if(light != null)
+        initializeLightSensor()
+        initializeGyroscopeSensor()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(this, mGyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager!!.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+
+    private fun initializeLightSensor() {
+        lightValue = findViewById<TextView>(R.id.light_id)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mLightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+        if(mLightSensor != null)
         {
-            sensorManager!!.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager!!.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL)
         }else
         {
-            lightValue?.text = "Light sensor not supported";
+            lightValue?.text = "Light sensor not supported"
         }
+    }
+
+    private fun initializeGyroscopeSensor() {
+        gyroscopeX = findViewById<TextView>(R.id.gyroscope_x_id)
+        gyroscopeY = findViewById<TextView>(R.id.gyroscope_y_id)
+        gyroscopeZ = findViewById<TextView>(R.id.gyroscope_z_id)
+        mGyroscopeSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
@@ -37,6 +64,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val sensor: Sensor = event.sensor
         if (sensor.type === Sensor.TYPE_LIGHT) {
             lightValue?.text = "Light Intensity: " + event.values[0]
+        }
+        if (sensor.type === Sensor.TYPE_GYROSCOPE) {
+            gyroscopeX?.text = "Gyroscope X: " + event.values[0]
+            gyroscopeY?.text = "Gyroscope Y: " + event.values[1]
+            gyroscopeZ?.text = "Gyroscope Z: " + event.values[2]
         }
     }
 }
